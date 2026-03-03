0a700f4ba9e938c76334bce4cec8db539f01abf9
Siva2109 <hack_2109@yahoo.com>
2026-03-03 11:35:36 +0800

    Added TamilGun and updated Playback 1.0.7

--- plugin.video.sivadelight/main.py
+++ plugin.video.sivadelight/main.py
@@ -13,7 +13,9 @@ def get_movies(site_url, site_name):
     try:
         response = requests.get(site_url, headers=headers, timeout=10)
         soup = BeautifulSoup(response.text, 'html.parser')
-        items = soup.find_all('article')
+        
+        # This looks for movie items on both TamilBulb and TamilGun
+        items = soup.find_all(['article', 'div'], class_=['video', 'v-item', 'post'])
         
         for item in items:
             link_tag = item.find('a')
@@ -23,46 +25,49 @@ def get_movies(site_url, site_name):
                 link = link_tag['href']
                 thumb = img_tag.get('data-src') or img_tag.get('src')
                 
-                list_item = xbmcgui.ListItem(label=title)
+                # Fix image links
+                if thumb and thumb.startswith('//'): thumb = 'https:' + thumb
+                
+                list_item = xbmcgui.ListItem(label=f"[{site_name}] {title}")
                 list_item.setArt({'thumb': thumb, 'icon': thumb, 'poster': thumb})
                 list_item.setInfo('video', {'title': title})
-                
-                # IMPORTANT CHANGE: We tell Kodi this item is PLAYABLE
                 list_item.setProperty('IsPlayable', 'true')
                 
-                # Create a link that points back to our "play" mode
                 query = urllib.parse.urlencode({'mode': 'play', 'url': link})
                 u = f"{sys.argv[0]}?{query}"
                 xbmcplugin.addDirectoryItem(handle=HANDLE, url=u, listitem=list_item, isFolder=False)
-
-    except Exception as e:
-        xbmcgui.Dialog().notification("SivaDelight", "Scraper Error")
+    except:
+        xbmcgui.Dialog().notification("SivaDelight", "Error loading site")
     xbmcplugin.endOfDirectory(HANDLE)
 
 def play_video(video_url):
-    # This is the "Magic" part used by Deccan Delight
-    # It sends the website link to ResolveURL to get the real video file
+    # This sends the link to ResolveURL. 
+    # TIP: If it fails, try clicking a different movie; some links expire!
     try:
         resolved_link = resolveurl.resolve(video_url)
         if resolved_link:
             play_item = xbmcgui.ListItem(path=resolved_link)
             xbmcplugin.setResolvedUrl(HANDLE, True, listitem=play_item)
         else:
-            xbmcgui.Dialog().ok("SivaDelight", "Could not resolve video link.")
+            xbmcgui.Dialog().ok("SivaDelight", "Video link not supported yet.")
     except Exception as e:
-        xbmcgui.Dialog().ok("SivaDelight", f"Play Error: {str(e)}")
+        xbmcgui.Dialog().ok("SivaDelight", "Playback Error")
 
 if __name__ == '__main__':
     params = dict(urllib.parse.parse_qsl(sys.argv[2][1:])) if sys.argv[2] else {}
     mode = params.get('mode')
     
     if not mode:
-        # Show the main menu (TamilBulb)
-        list_item = xbmcgui.ListItem(label="TamilBulb Movies")
-        query = urllib.parse.urlencode({'mode': 'list', 'url': 'https://tamilbulb.cc/'})
-        xbmcplugin.addDirectoryItem(handle=HANDLE, url=f"{sys.argv[0]}?{query}", listitem=list_item, isFolder=True)
+        # MAIN MENU: Now with TamilGun!
+        add_menu_item("TamilGun (New)", "https://tamilgun.now/", "list")
+        add_menu_item("TamilBulb", "https://tamilbulb.cc/", "list")
         xbmcplugin.endOfDirectory(HANDLE)
     elif mode == 'list':
-        get_movies(params['url'], "TamilBulb")
+        get_movies(params['url'], "Movies")
     elif mode == 'play':
         play_video(params['url'])
+
+def add_menu_item(name, url, mode):
+    list_item = xbmcgui.ListItem(label=name)
+    query = urllib.parse.urlencode({'mode': mode, 'url': url})
+    xbmcplugin.addDirectoryItem(handle=HANDLE, url=f"{sys.argv[0]}?{query}", listitem=list_item, isFolder=True)
