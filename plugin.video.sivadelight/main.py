dd3f2113b1656803cbee70a8c0b63570174ecd2e
Siva2109 <hack_2109@yahoo.com>
2026-03-03 10:05:24 +0800

    Real scraper logic added

--- plugin.video.sivadelight/main.py
+++ plugin.video.sivadelight/main.py
@@ -1,53 +1,45 @@
 import sys
-import urllib.parse
 import xbmcgui
 import xbmcplugin
 import requests
 from bs4 import BeautifulSoup
 
+# This is the "ID" Kodi uses to track this menu
 HANDLE = int(sys.argv[1])
-BASE_URL = "https://tamilyogi.plus/" # You can change this when domains change
 
-def build_menu():
-    # Folder for New Movies
-    add_directory_item("Latest Tamil Movies", "scrape_site", BASE_URL + "category/tamil-new-movies/")
-    xbmcplugin.endOfDirectory(HANDLE)
-
-def scrape_movies(url):
-    # This is the "Engine"
+def get_movies():
+    url = 'https://tamilyogi.plus/' 
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
     
-    # This logic depends on the website's HTML structure
-    for movie in soup.find_all('div', class_='cover'):
-        title = movie.find('img')['alt']
-        link = movie.find('a')['href']
-        img = movie.find('img')['src']
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        soup = BeautifulSoup(response.text, 'html.parser')
         
-        # Add the movie to the Kodi list
-        add_video_item(title, link, img)
-    
+        # TamilYogi usually puts movies in 'video' class list items
+        movies = soup.find_all('li', class_='video')
+        
+        for movie in movies:
+            # Get the title and the link
+            title_tag = movie.find('div', class_='title')
+            link_tag = movie.find('a')
+            img_tag = movie.find('img')
+            
+            if title_tag and link_tag:
+                title = title_tag.text.strip()
+                link = link_tag['href']
+                thumb = img_tag['src'] if img_tag else ""
+                
+                list_item = xbmcgui.ListItem(label=title)
+                list_item.setArt({'thumb': thumb, 'icon': thumb})
+                list_item.setInfo('video', {'title': title, 'plot': 'TamilYogi Latest'})
+                
+                # isFolder=True because the movie page is another menu of quality links
+                xbmcplugin.addDirectoryItem(handle=HANDLE, url=link, listitem=list_item, isFolder=True)
+
+    except Exception as e:
+        xbmcgui.Dialog().ok("SivaDelight Error", str(e))
+
     xbmcplugin.endOfDirectory(HANDLE)
-
-def add_directory_item(name, action, url):
-    path = f"{sys.argv[0]}?action={action}&url={urllib.parse.quote_plus(url)}"
-    list_item = xbmcgui.ListItem(label=name)
-    xbmcplugin.addDirectoryItem(handle=HANDLE, url=path, listitem=list_item, isFolder=True)
-
-def add_video_item(name, url, img):
-    list_item = xbmcgui.ListItem(label=name)
-    list_item.setArt({'thumb': img, 'icon': img})
-    # This tells Kodi it's a playable video
-    list_item.setProperty('IsPlayable', 'true')
-    xbmcplugin.addDirectoryItem(handle=HANDLE, url=url, listitem=list_item, isFolder=False)
 
 if __name__ == '__main__':
-    params = dict(urllib.parse.parse_qsl(sys.argv[2][1:]))
-    action = params.get('action')
-    url = params.get('url')
-
-    if not action:
-        build_menu()
-    elif action == 'scrape_site':
-        scrape_movies(url)
+    get_movies()
